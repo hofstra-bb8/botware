9a13926204fa5dabca9f28baba293dcede376f56
Milan Iliev <milaniliev@gmail.com>
2016-04-29 14:57:29 -0400

    Attempt at routine

--- botware.py
+++ botware.py
@@ -2,19 +2,29 @@ from pygame import mixer # Load the required library
 
 from dual_mc33926_rpi import motors, MAX_SPEED
 import time
-
 mixer.init()
 sound = mixer.music.Sound('./bb8 - he he he ho.wav')
 
-sound.play()
+def make_noise():
+  sound.play()
 
-while mixer.get_busy():
-  sleep(1)
+  while mixer.get_busy():
+    sleep(1)
+
+def forward(seconds):
+  motors.setSpeeds(seconds, seconds)
+
+def reverse(seconds):
+  motors.setSpeeds(-seconds, -seconds)
+
 
 try:
   motors.enable()
-  # motors.setSpeeds(MAX_SPEED, MAX_SPEED)
-  time.sleep(10)
+
+  forward(10)
+  make_noise()
+  reverse(2)
+  make_noise()
   
 finally:
   motors.setSpeeds(0,0)
