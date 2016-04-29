21f3bf511076ca22912336db86231434d7c177cc
Milan Iliev <milaniliev@gmail.com>
2016-04-28 21:44:45 -0400

    Added sleep()

--- botware.py
+++ botware.py
@@ -1,6 +1,7 @@
 from pygame import mixer # Load the required library
 
 from dual_mc33926_rpi import motors, MAX_SPEED
+import time
 
 mixer.init()
 mixer.music.load('./bb8.mp3')
@@ -9,6 +10,7 @@ mixer.music.play()
 try:
   motors.enable()
   motors.setSpeeds(MAX_SPEED, MAX_SPEED)
+  time.sleep(10)
   
 finally:
   motors.setSpeeds(0,0)
