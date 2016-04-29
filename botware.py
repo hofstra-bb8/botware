8316ec11d91b8c615538eea261a7e759b65c1d4c
Milan Iliev <milaniliev@gmail.com>
2016-04-28 21:37:59 -0400

    Added motors and sound code

---
+++ botware.py
@@ -0,0 +1,15 @@
+from pygame import mixer # Load the required library
+
+from dual_mc33926_rpi import motors, MAX_SPEED
+
+mixer.init()
+mixer.music.load('./bb8.m4r')
+mixer.music.play()
+
+try:
+  motors.enable()
+  motors.setSpeeds(MAX_SPEED, MAX_SPEED)
+  
+finally:
+  motors.setSpeeds(0,0)
+  motors.disable()
